3fcc7a77c2d158676d330939227b3c535f60b276
khushbu <khushbuthakkar1612@gmail.com>
2020-04-05 21:52:12 +0530

    Updates

---
+++ precache-manifest.c12393fab74c333e119032f6ddb5fdbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0860e39e0c59ca4850cf1f61eba60a5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72b9e3f41a1d0731360b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f3be4d1b2e36dc00a132",
+    "url": "/static/js/2.fb767a66.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fb767a66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72b9e3f41a1d0731360b",
+    "url": "/static/js/main.1d7be11c.chunk.js"
+  },
+  {
+    "revision": "f70af41a2d1512219933",
+    "url": "/static/js/runtime-main.7760e9f9.js"
+  }
+]);
